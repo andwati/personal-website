d29fe9c6c54106b41178cbc6a2a7e01105e0dfc0
andwati <andwatiian@gmail.com>
2026-02-14 16:16:07 +0300

    feat: Add Nuxt SEO LLM resources and configure project for SEO modules.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/content"],
+	modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/content", "nuxt-studio"],
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
 	compatibilityDate: "2024-04-03",
